9df3d2348737ffacdaf0beea6f43c67dfa93415d
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-01-28 01:03:51 +0000

    - allowing user to enter a city name into the text box and searching the city inputted - adding searched city to localstorage

--- starter/js/logic.js
+++ starter/js/logic.js
@@ -1,39 +1,24 @@
-
-// var apiKey = '7aef1906e93e8116cc215bc5377288b3'
-// var apiCall = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=' + apiKey
-// console.log(apiCall)
-// $('#search-button').on("click", function () {
-// fetch(apiCall)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data)
-//     });
-
-// });
-
 $('#search-button').on("click", function (event) {
-event.preventDefault(); 
-var apiKey = '7aef1906e93e8116cc215bc5377288b3';
-var getCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=' + apiKey;
+    var searchWord = $('#search-input').val().trim();
+    localStorage.setItem('searchWord', searchWord)
+    event.preventDefault();
+    var apiKey = '7aef1906e93e8116cc215bc5377288b3';
+    var getCoordinates = 'http://api.openweathermap.org/geo/1.0/direct?q='+searchWord+'&limit=5&appid=' + apiKey;
 
-fetch(getCoordinates)
-.then(function(response) {
-  return response.json();
-}).then(function(data) {
-  console.log(data);
-var lat = data[0].lat
-var lon = data[0].lon
-  var getWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid=' + apiKey
-  fetch(getWeather)
-.then(function(response) {
-  return response.json();
-}).then(function(data) {
-  console.log('weather data', data);
-});
-});
+    fetch(getCoordinates)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            var lat = data[0].lat
+            var lon = data[0].lon
+            var getWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey
+            fetch(getWeather)
+                .then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    console.log('weather data', data);
+                });
+        });
 
-// prevents form from submitting
     console.log('test')
 });
